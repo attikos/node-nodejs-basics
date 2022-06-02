49a84077c867a1f15a77afe59ec16baca993363b
Bylkov Maksim <bylkov@atri.tech>
2022-06-03 01:20:55 +0400

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+
+    const res = [];
+    args.forEach((x, i) => {
+        if (i % 2) {
+            res.push(`${args[i - 1]} is ${x}`)
+        }
+    })
+
+    console.log(res.join(', '));
 };
+
+parseArgs()
