26a0653879ce266557a8529dded27f7145cf72ba
Bylkov Maksim <bylkov@atri.tech>
2022-06-03 02:30:25 +0400

    fix: streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,9 +1,21 @@
-export const transform = async () => {
-    process.stdin.on('data', data => {
-        const res = data.toString().split('').reverse().join('') + '\n';
+import { Transform } from 'stream'
 
-        process.stdout.write(res);
-    })
+export const transform = async () => {
+    // ver. 1
+    // process.stdin.on('data', data => {
+    //     const res = data.toString().split('').reverse().join('') + '\n';
+
+    //     process.stdout.write(res);
+    // })
+
+    // ver. 2
+    const revert = new Transform({
+      transform(chunk, encoding, callback) {
+        callback(null, chunk.toString().split('').reverse().join('') + '\n');
+      },
+    });
+
+    process.stdin.pipe(revert).pipe(process.stdout)
 }
 
 transform()
